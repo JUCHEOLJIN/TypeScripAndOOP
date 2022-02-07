780d690453800781d8a0df3aca2b3360c89359ea
eden.ju <public.jin03@gmail.com>
2022-02-07 23:57:20 +0900

    feat: discriminated Union 정리

---
+++ 1-types/1-6-discriminated.ts
@@ -0,0 +1,37 @@
+{
+  // function: login -> success, fail 🕡
+  type SuccessState = {
+    result: "success";
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    result: "fail";
+    reason: string;
+  };
+
+  type LoginState = SuccessState | FailState;
+
+  function login(id: string, password: string): LoginState {
+    return {
+      result: "success",
+      response: {
+        body: "logged in!",
+      },
+    };
+  }
+
+  // printLoginState(state)
+  // success -> 🎉 body
+  // fail -> 😂 reason
+
+  function printLoginState(state: LoginState) {
+    if (state.result === "success") {
+      console.log(`🎉 ${state.response.body}`);
+    } else {
+      console.log(`😂 ${state.reason}`);
+    }
+  }
+}
