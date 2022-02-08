5d974a16f3be8e6fdb2a58ce0705f69796ec277d
eden.ju <public.jin03@gmail.com>
2022-02-08 22:02:54 +0900

    feat: 좌표게임 추가 및 블록 스코프 추가

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,38 @@
-/**
- * Let's make a game 🕹
- */
-console.log(position); // { x: 0, y: 0}
-move('up');
-console.log(position); // { x: 0, y: 1}
-move('down');
-console.log(position); // { x: 0, y: 0}
-move('left');
-console.log(position); // { x: -1, y: 0}
-move('right');
-console.log(position); // { x: 0, y: 0}
+{
+  /**
+   * Let's make a game 🕹
+   */
+
+  type CommandType = "up" | "down" | "left" | "right";
+
+  const position: { x: number; y: number } = { x: 0, y: 0 };
+
+  function move(command: CommandType) {
+    switch (command) {
+      case "up":
+        position.y++;
+        break;
+      case "down":
+        position.y--;
+        break;
+      case "left":
+        position.x--;
+        break;
+      case "right":
+        position.x++;
+        break;
+      default:
+        throw new Error("unknown command");
+    }
+  }
+
+  console.log(position); // { x: 0, y: 0}
+  move("up");
+  console.log(position); // { x: 0, y: 1}
+  move("down");
+  console.log(position); // { x: 0, y: 0}
+  move("left");
+  console.log(position); // { x: -1, y: 0}
+  move("right");
+  console.log(position); // { x: 0, y: 0}
+}
