de54bb8420c0b0d5e9be9059f6eaf8fa5608213f
eden.ju <public.jin03@gmail.com>
2022-02-12 20:55:11 +0900

    feat: constrains 추가

--- 5-generic/5-3-constrains.ts
+++ 5-generic/5-3-constrains.ts
@@ -37,4 +37,21 @@
 
   const edenAfterPay = pay(eden);
   const bbAfterPay = pay(bb);
+
+  const obj = {
+    name: "eden",
+    age: 20,
+  };
+
+  const obj2 = {
+    animal: "monkey",
+  };
+
+  function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+  }
+
+  console.log(getValue(obj, "name")); // eden
+  console.log(getValue(obj, "age")); // 20
+  console.log(getValue(obj2, "animal")); // "monkey"
 }
