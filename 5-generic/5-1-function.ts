a9434f6ad3ca24d07814844f0cdbe63d76224952
eden.ju <public.jin03@gmail.com>
2022-02-12 18:27:44 +0900

    feat: 함수 제네릭 정리

---
+++ 5-generic/5-1-function.ts
@@ -0,0 +1,28 @@
+{
+  function checkNotNullBad(arg: number | null): number {
+    if (arg == null) {
+      throw new Error("not valid number!");
+    }
+
+    return arg;
+  }
+
+  function checkNotNullAnyBad(arg: any | null): any {
+    if (arg == null) {
+      throw new Error("not valid number!");
+    }
+
+    return arg;
+  }
+
+  function checkNotNull<T>(arg: T | null): T {
+    if (arg == null) {
+      throw new Error("not valid number!");
+    }
+
+    return arg;
+  }
+
+  const number = checkNotNull(123);
+  const boal: boolean = checkNotNull(true);
+}
