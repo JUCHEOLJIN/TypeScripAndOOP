843ab18038400db18292dc8c019947db04b06a37
eden.ju <public.jin03@gmail.com>
2022-02-14 23:37:46 +0900

    feat: 기본 에러 핸들링 정리

--- 8-exception/8-1-exeption.ts
+++ 8-exception/8-1-exeption.ts
@@ -1,4 +1,30 @@
 // Java : Exception
 // JavaScript: Error
 
-const array = new Array(10000000000000);
+// const array = new Array(10000000000000);
+
+// Error(Exception) Handling: try -> catch -> finally
+
+function readFile(fileName: string): string {
+  if (fileName === "not exist!💩") {
+    throw new Error(`file not exist! ${fileName}`);
+  }
+  return "file contents🗒";
+}
+
+function closeFile(fileName: string) {}
+
+function run() {
+  const fileName = "not exist!💩";
+
+  try {
+    console.log(readFile(fileName));
+  } catch (error) {
+    console.log(`catched!!`);
+  } finally {
+    closeFile(fileName);
+    console.log(`closed!`);
+  }
+}
+
+run();
