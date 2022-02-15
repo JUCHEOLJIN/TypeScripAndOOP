c1f422c5bf4934faf2c0f2f77b3684016bbf009a
eden.ju <public.jin03@gmail.com>
2022-02-16 00:15:12 +0900

    feat: 인덱스 타입 정리

---
+++ 9-advanced/9-2-index.ts
@@ -0,0 +1,20 @@
+{
+  type Animal = {
+    name: string;
+    age: number;
+    gender: "male" | "female";
+  };
+
+  type Name = Animal["name"]; // string
+  const text: Name = "hello";
+
+  type Gender = Animal["gender"]; // "male" | "female"
+
+  type Keys = keyof Animal; // "name" | "age" | "gender"
+  const key: Keys = "gender";
+
+  type Person = {
+    name: string;
+    gender: Animal["gender"];
+  };
+}
